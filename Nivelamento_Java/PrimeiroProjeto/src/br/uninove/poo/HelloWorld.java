9671bf4da9204606f70dec07eee5c7056f65c3f0
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-08-19 21:57:33 -0300

    Primeiro projeto

---
+++ Nivelamento_Java/PrimeiroProjeto/src/br/uninove/poo/HelloWorld.java
@@ -0,0 +1,22 @@
+//isso é um comentário em Java
+
+/*  
+    Isso também é um comentário em Java
+    porém em bloco!
+    Muito legal, não!?
+*/
+
+package br.uninove.poo;
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Olá \"Uninove\"");
+        System.out.println("Meu nome é Traue!");
+        
+        //em seguida começaremos a aprender sobre 
+        //declaração de variáveis
+        //porém... aula que bem :(
+    } 
+
+}
