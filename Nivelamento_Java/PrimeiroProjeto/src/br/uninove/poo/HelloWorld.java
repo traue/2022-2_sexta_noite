98f49c1aff82562b46aa91769b9841055c85fcf2
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-08-26 21:57:59 -0300

    Continuação

--- Nivelamento_Java/PrimeiroProjeto/src/br/uninove/poo/HelloWorld.java
+++ Nivelamento_Java/PrimeiroProjeto/src/br/uninove/poo/HelloWorld.java
@@ -1,22 +1,44 @@
-//isso é um comentário em Java
-
-/*  
-    Isso também é um comentário em Java
-    porém em bloco!
-    Muito legal, não!?
-*/
-
 package br.uninove.poo;
 
 public class HelloWorld {
     
+    //Isso é um comentário
+    /*
+        Isso é um bloco de
+        comentário! :D
+    */
+    
     public static void main(String[] args) {
-        System.out.println("Olá \"Uninove\"");
-        System.out.println("Meu nome é Traue!");
+        System.out.println("Iniciando nosso primeiro projeto!");
         
-        //em seguida começaremos a aprender sobre 
-        //declaração de variáveis
-        //porém... aula que bem :(
-    } 
+        String nome = "Thiago G. Traue";
+        char sexo = 'N';
+        int anoNascimento = 1987;
+        //java usa normalmente operações matemáticas:
+        int idade = 2022 - anoNascimento; 
+        float altura = 1.83f;
+        boolean fumante = false;
+        boolean temCarroAntigo = true;
+        
+        //imprimindo as informações:
+        System.out.println("Olá " + nome + ", tudo bem?");
+        System.out.println("Idade informada: " + idade);
+        System.out.println("Altura informada: " + altura);
+        System.out.println("Sexo informado: " + sexo);
+        //System.out.println("É fumante? " + fumante);
+        
+        //desvio condicional do Java
+        if(fumante) {
+            System.out.println("Fumante: Sim");
+        } else {
+            System.out.println("Fumante: Não");
+        }
+        
+        if(temCarroAntigo) {
+            System.out.println("Antigomobilista!");
+        } else {
+            System.out.println("Carrinho moderninho hein!");
+        }
+    }
 
 }
---
+++ Nivelamento_Java/PrimeiroProjeto/src/br/uninove/poo/MiniCalculadora.java
@@ -0,0 +1,38 @@
+package br.uninove.poo;
+
+import java.util.Scanner;
+
+
+/*
+    A ideia aqui é criar uma mini calculadora
+    onde o usuário digita dois valores 
+    e é exibido à ele as 4 operações sobre estes 
+    valores além do resto da divisão :D
+*/
+public class MiniCalculadora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+           
+        float v1, v2, soma, sub, mult, div, resto;
+        
+        System.out.print("Informe o valor 1: ");
+        v1 = sc.nextFloat();
+        
+        System.out.print("Informe o valor 2: ");
+        v2 = sc.nextFloat();
+        
+        soma = v1 + v2;
+        sub = v1 - v2;
+        mult = v1 * v2;
+        
+        if(v2 == 0) {
+            System.out.println("A divisão não pode ser calculada!");
+        } else {
+            div = v1 / v2;
+        }
+        
+        //TENTEM TERMINAR ESTE CÓDIGO PARA SEMANA QUE VEM :D
+        //FALTA: EXIBIR CADA UM DOS RESULTADOS (AINDA NÃO CALCULAMOS O RESTO!)
+        
+    }
+}
