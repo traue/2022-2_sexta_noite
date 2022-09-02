3aa86a341bd30cd004faaf4cda71681375a70f22
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-09-02 20:12:00 -0300

    Update MiniCalculadora.java

--- Nivelamento_Java/PrimeiroProjeto/src/br/uninove/poo/MiniCalculadora.java
+++ Nivelamento_Java/PrimeiroProjeto/src/br/uninove/poo/MiniCalculadora.java
@@ -8,33 +8,35 @@ import java.util.Scanner;
     onde o usuário digita dois valores 
     e é exibido à ele as 4 operações sobre estes 
     valores além do resto da divisão :D
-*/
+ */
 public class MiniCalculadora {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-           
+
         float v1, v2, soma, sub, mult, div, resto;
-        
+
         System.out.print("Informe o valor 1: ");
         v1 = sc.nextFloat();
-        
+
         System.out.print("Informe o valor 2: ");
         v2 = sc.nextFloat();
-        
+
         soma = v1 + v2;
         sub = v1 - v2;
         mult = v1 * v2;
-        
-        if(v2 == 0) {
+
+        System.out.println("A soma é: " + soma);
+        System.out.println("A subtração é: " + sub);
+        System.out.println("A multiplicação é: " + mult);
+
+        if (v2 == 0) {
             System.out.println("A divisão não pode ser calculada!");
         } else {
             div = v1 / v2;
+            resto = v1 % v2;
+            System.out.println("A divisão é: " + div);
+            System.out.println("O resto da divisão é: " + resto);
         }
-        
-        //TENTEM TERMINAR ESTE CÓDIGO PARA SEMANA QUE VEM :D
-        //FALTA: EXIBIR CADA UM DOS RESULTADOS (AINDA NÃO CALCULAMOS O RESTO!)
-        
     }
 }
-
-//DESAFIO 2: IMPLEMENTAR UMA DESSA PARA IMC (com a classificação!)
